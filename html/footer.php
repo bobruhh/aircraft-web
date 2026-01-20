1b12038764a020c15ad85bc100e216ec4836041b
bobruhh <zablockasg@gmail.com>
2026-01-21 00:26:57 +0200

    Delete html directory

--- html/footer.php
+++
@@ -1,7 +0,0 @@
-</div>
-<footer style="text-align:center; padding:15px; font-size:14px; color:grey; border-top:1px solid #ccc; background:white;">
-   <?= date('Y') ?> IT Projektas · Sukurta Gustas Zablockas IFB-3
-</footer>
-</body>
-</html>
-
